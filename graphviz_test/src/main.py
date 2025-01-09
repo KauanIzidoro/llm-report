42b8758e9f0e2d40ab98a19a20b1910658d9d25f
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-09 14:58:19 -0300

    :pencil: refactor layout of der

--- graphviz_test/src/main.py
+++ graphviz_test/src/main.py
@@ -1,11 +1,57 @@
 from graphviz import Digraph
 
+der = Digraph('Diagrama Entidade-Relacionamento', node_attr={'shape': 'plaintext'})
 
-dot = Digraph(comment="Exemplo de Grafo")
-dot.node('A', 'Etapa 1')
-dot.node('B', 'Etapa 2')
-dot.node('C', 'Etapa 3')
-dot.edge('A', 'B', 'Transição')
-dot.edge('B', 'C', 'Concluir')
-dot.render('diagrama_teste', format='png', cleanup=True)  
+der.node('Cliente', '''<
+    <table border="1" cellborder="1" cellspacing="0">
+        <tr><td bgcolor="lightblue"><b>Cliente</b></td></tr>
+        <tr><td>ID_Cliente</td></tr>
+        <tr><td>Nome</td></tr>
+        <tr><td>Email</td></tr>
+    </table>
+>''')
 
+der.node('Pedido', '''<
+    <table border="1" cellborder="1" cellspacing="0">
+        <tr><td bgcolor="lightblue"><b>Pedido</b></td></tr>
+        <tr><td>ID_Pedido</td></tr>
+        <tr><td>Data</td></tr>
+        <tr><td>Valor</td></tr>
+    </table>
+>''')
+
+der.node('Produto', '''<
+    <table border="1" cellborder="1" cellspacing="0">
+        <tr><td bgcolor="lightblue"><b>Produto</b></td></tr>
+        <tr><td>ID_Produto</td></tr>
+        <tr><td>Nome</td></tr>
+        <tr><td>Preço</td></tr>
+    </table>
+>''')
+
+der.node('Estoque', '''<
+         <table border="1" cellborder="1" cellspacing="0">
+            <tr><td bgcolor="lightblue"><b>Estoque</b></td></tr>
+            <tr><td>ID_Produto</td></tr>
+            <tr><td>Nome</td></tr>
+            <tr><td>Preço</td></tr>
+            <tr><td>Hora</td></tr>
+         </table>
+         >''')
+
+der.node('Faz', '''<
+    <table border="1" cellborder="1" cellspacing="0">
+        <tr><td bgcolor="lightgrey"><b>Faz</b></td></tr>
+    </table>
+>''', shape='none')
+der.node('Contém', '''<
+    <table border="1" cellborder="1" cellspacing="0">
+        <tr><td bgcolor="lightgrey"><b>Contém</b></td></tr>
+    </table>
+>''', shape='none')
+
+der.edge('Cliente', 'Faz', label='1', arrowhead='none')
+der.edge('Faz', 'Pedido', label='N', arrowhead='none')
+der.edge('Pedido', 'Contém', label='1', arrowhead='none')
+der.edge('Contém', 'Produto', label='N', arrowhead='none')
+der.render('resultados/der_tabelas', format='png', cleanup=True)
