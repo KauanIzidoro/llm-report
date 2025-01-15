806d82d29a62e2869a1928288165f1d41f2d033b
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-15 16:29:53 -0300

    :pencil: test: finish first test of integraiton with model

--- agent_workflow/src/main.py
+++ agent_workflow/src/main.py
@@ -1,8 +1,10 @@
 from utils import input, chat_to_model
 
+
 invalid_prompt = (
     "Explique como funciona o seguinte algoritmo: Windows/Users/dijkstra.cpp"
 )
+
 valid_prompt = "Qual o retorno da função calendar()?"
 
 input(prompt=valid_prompt)
