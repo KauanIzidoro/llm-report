f302789ca9fc8e989c029d6a08b8872509d12a4f
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-15 16:29:02 -0300

    :pencil: refactor 'chat_to_model' for dont use cache storage

--- agent_workflow/src/utils.py
+++ agent_workflow/src/utils.py
@@ -2,7 +2,6 @@ import re
 import os
 from settings import SETTINGS
 import google.generativeai as genai
-from google.generativeai import caching
 
 
 def input(prompt: str):
@@ -31,23 +30,24 @@ def chat_to_model(
         context (optional): Defaults to SETTINGS.PROJECT_PATH.
         model_output (optional): Defaults to SETTINGS.OUTPUT_PATH.
     """
-    with open(context, "r") as c:
-        context_text = c.read()
-
-    genai.configure(api_key=SETTINGS.GOOGLE_API_KEY)
-    cache = caching.CachedContent.create(
-        model=SETTINGS.MODEL,
-        system_instruction=(
-            "You are a software engineer and your goal is to explain the source code to new squad members."
-        ),
-        contents=context_text,
-    )
-    model = genai.GenerativeModel.from_cached_content(cached_content=cache)
     try:
-        response = model.generate_content([prompt])
+        with open(context, "r", encoding="utf-8") as c:
+            context_text = c.read()
+
+        combined_prompt = f"{context_text}\n\n{prompt}"
+
+        genai.configure(api_key=SETTINGS.GOOGLE_API_KEY)
+        model = genai.GenerativeModel(model_name=SETTINGS.MODEL, 
+                                      system_instruction="Você é um engenheiro de software e seu objetivo é explicar o código-fonte para novos membros da equipe.")
+
+        response = model.generate_content([combined_prompt])
         output = response.text
+
         with open(model_output, "w", encoding="utf-8") as file:
             file.write(output)
+
+        return output
+
     except Exception as e:
         print(f"Erro ao gerar a resposta: {e}")
-    return output
+        return None
