63e6780781d4947f6953c97a018c069087f32ae3
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-19 20:54:38 -0300

    :sparkles: feat: create settings class

--- doc_agent_api/src/settings.py
+++ doc_agent_api/src/settings.py
@@ -1,1 +1,14 @@
-#
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+class Settings(BaseSettings):
+    PROJECT_PATH: str
+    OUTPUT_PATH: str
+    GOOGLE_API_KEY: str
+    MODEL: str 
+    API_PORT: str
+    
+
+SETTINGS = Settings()
+    
