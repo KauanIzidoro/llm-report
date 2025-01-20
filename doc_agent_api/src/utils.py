893498cfe3ab85d51b36db38bf811bf20755ee76
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-19 21:35:36 -0300

    :sparkles: feat: add google api dependence and create input validation function

--- doc_agent_api/src/utils.py
+++ doc_agent_api/src/utils.py
@@ -1,1 +1,25 @@
-#
+import re 
+import os 
+from settings import SETTINGS
+import time
+import google.generativeai as genai
+
+
+def validate_input(text_prompt: str):
+    """_summary_
+    Args:
+        text_prompt (str): texto que seja usado como prompt para interagir com o modelo.
+
+    Raises:
+        Exception: Prompts que forem caminhos de arquivos ou imagens não são permitidos.
+
+    Returns:
+        str: prompt se for válido.
+    """
+    if re.search(os.sep, text_prompt) or re.search(r"\.(png|svg|jpg)$", text_prompt):
+        raise Exception("O prompt deve ser texto!")
+    return text_prompt
+
+
+        
+    
