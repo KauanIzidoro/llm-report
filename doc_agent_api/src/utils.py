e7f230d2cdd0b2426c7818bb2399cc4befd035c3
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-19 21:36:28 -0300

    :sparkles: feat: create function for chat to model api

--- doc_agent_api/src/utils.py
+++ doc_agent_api/src/utils.py
@@ -20,6 +20,34 @@ def validate_input(text_prompt: str):
         raise Exception("O prompt deve ser texto!")
     return text_prompt
 
+def chat_to_model(
+    prompt: str, context=SETTINGS.PROJECT_PATH, model_output=SETTINGS.OUTPUT_PATH
+):
+    """_summary_
 
+    Args:
+        prompt (str): texto que seja usado como prompt para interagir com o modelo.
+        context (optional): Defaults to SETTINGS.PROJECT_PATH.
+        model_output (optional): Defaults to SETTINGS.OUTPUT_PATH.
+    """
+    try:
+        with open(context, 'r', encoding='utf-8') as c:
+            context_text = c.read()
+            
+        context_prompt = f'{context_text}\n\n{prompt}'
+        
+        genai.configure(api_key=SETTINGS.GOOGLE_API_KEY)
+        model = genai.GenerationModel(model_name=SETTINGS.MODEL,
+                                      system_instruction=SETTINGS.SYSTEM_INSTRUCTION)
+        response = model.generate_content([context_prompt])
+        output = f'{time.acstime}\n\n{response.text}\n\n'
+        
+        with open(model_output, 'w', encoding='utf-8') as file:
+            file.write(output)
+        
+        return output
+    except Exception as e:
+        print(f'Erro ao gerar resposta: {e}')
+        return None
         
     
