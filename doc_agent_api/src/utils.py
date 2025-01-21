bdb5d538bc6fc1f32fa6f7b97b30b42054aed1ab
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-21 02:48:21 -0300

    :sparkles: feat: add new functions for rest communication

--- doc_agent_api/src/utils.py
+++ doc_agent_api/src/utils.py
@@ -1,9 +1,10 @@
-import re 
+import os
+import re
 import time
-import os 
-from settings import SETTINGS
-import time
+
 import google.generativeai as genai
+
+from settings import SETTINGS
 
 
 def validate_input(text_prompt: str):
@@ -17,30 +18,70 @@ def validate_input(text_prompt: str):
     Returns:
         str: prompt se for válido.
     """
-    if re.search(os.sep, text_prompt) or re.search(r"\.(png|svg|jpg)$", text_prompt):
-        raise Exception("O prompt deve ser texto!")
+    if re.search(os.sep, text_prompt) or re.search(
+        r'\.(png|svg|jpg)$', text_prompt
+    ):
+        raise Exception('O prompt deve ser texto!')
     return text_prompt
 
 
 def mock_chat_to_model(
-    prompt: str, context=SETTINGS.PROJECT_PATH, model_output=SETTINGS.OUTPUT_PATH
+    prompt: str,
+    context=SETTINGS.PROJECT_PATH,
+    model_output=SETTINGS.OUTPUT_PATH,
 ):
     try:
         with open(context, 'r', encoding='utf-8') as c:
             context_text = c.read()
-            
+
         context_prompt = f'{context_text}\n\n{prompt}'
         with open(model_output, 'a', encoding='utf-8') as f:
             f.write(f'{time.ctime}\n{context_prompt}\n\n')
-        
+
         return context_prompt
     except Exception as e:
         print(f'Erro ao gerar resposta: {e}')
         return None
 
 
+def receive_input(prompt: str, input_file=SETTINGS.INPUT_PATH):
+    try:
+        with open(input_file, 'a', encoding='utf-8') as f:
+            f.write(f'{prompt}\n\n')
+
+        return prompt
+    except Exception as e:
+        print(f'Erro ao gerar resposta: {e}')
+        return None
+
+
+def chat_to_model_rest(
+    input_file=SETTINGS.INPUT_PATH,
+    model_output=SETTINGS.OUTPUT_PATH,
+    context=SETTINGS.PROJECT_PATH,
+):
+    try:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            prompt = f.read()
+
+        with open(context, 'r', encoding='utf-8') as c:
+            context_text = c.read()
+
+        context_prompt = f'{prompt}\n\n{context_text}'
+
+        with open(model_output, 'a', encoding='utf-8') as m:
+            m.write(f'{time.ctime}\n{context_prompt}\n\n')
+
+        return context_prompt
+    except Exception as e:
+        print(f'Erro: {e}')
+        return None
+
+
 def chat_to_model(
-    prompt: str, context=SETTINGS.PROJECT_PATH, model_output=SETTINGS.OUTPUT_PATH
+    prompt: str,
+    context=SETTINGS.PROJECT_PATH,
+    model_output=SETTINGS.OUTPUT_PATH,
 ):
     """_summary_
 
@@ -52,21 +93,21 @@ def chat_to_model(
     try:
         with open(context, 'r', encoding='utf-8') as c:
             context_text = c.read()
-            
+
         context_prompt = f'{context_text}\n\n{prompt}'
-        
+
         genai.configure(api_key=SETTINGS.GOOGLE_API_KEY)
-        model = genai.GenerationModel(model_name=SETTINGS.MODEL,
-                                      system_instruction=SETTINGS.SYSTEM_INSTRUCTION)
+        model = genai.GenerationModel(
+            model_name=SETTINGS.MODEL,
+            system_instruction=SETTINGS.SYSTEM_INSTRUCTION,
+        )
         response = model.generate_content([context_prompt])
         output = f'{time.acstime}\n\n{response.text}\n\n'
-        
+
         with open(model_output, 'w', encoding='utf-8') as file:
             file.write(output)
-        
+
         return output
     except Exception as e:
         print(f'Erro ao gerar resposta: {e}')
         return None
-
-    
