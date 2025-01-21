4bc16f23b6fb98339814b8426573380a905d2b52
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-21 02:48:47 -0300

    :sparkles: feat: update routes file for websocket communication

--- doc_agent_api/src/routes.py
+++ doc_agent_api/src/routes.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, WebSocket
 from fastapi.responses import HTMLResponse
-from utils import validate_input, mock_chat_to_model
+
+from utils import (
+    mock_chat_to_model,
+    validate_input,
+)
+
 router = APIRouter()
 
 html = """
@@ -37,45 +42,12 @@ html = """
 </html>
 """
 
-html_rest = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat4Vision</title>
-    </head>
-    <body>
-        <h1>DocAgentChat (REST)</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
 
 @router.get('/')
 def get():
     return HTMLResponse(html)
 
-    
+
 @router.websocket('/ws')
 async def rt_chat(ws: WebSocket):
     await ws.accept()
@@ -84,14 +56,3 @@ async def rt_chat(ws: WebSocket):
         validate_prompt = validate_input(text_prompt=text_prompt)
         model_output = mock_chat_to_model(prompt=validate_prompt)
         await ws.send_text(model_output)
-        
-@router.get('/rest')
-def rest_chat():
-    return HTMLResponse(html_rest)
-
-@router.post('/chat')
-def rest_chat(user_prompt: str):
-    validate_prompt = validate_input(text_prompt=user_prompt)
-    model_output = mock_chat_to_model(prompt=validate_prompt)
-    return {"response": model_output}
-    
