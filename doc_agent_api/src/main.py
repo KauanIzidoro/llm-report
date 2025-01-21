f06202d9a9e6ac7537e4fcaf8b0881a626d7dbaf
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-21 02:47:52 -0300

    :wrench: chore: update api name and add new env variables

--- doc_agent_api/src/main.py
+++ doc_agent_api/src/main.py
@@ -1,11 +1,11 @@
-import uvicorn 
-from settings import SETTINGS
+import uvicorn
 from fastapi import FastAPI
+
 from routes import router
+from settings import SETTINGS
 
 app = FastAPI(title='DocAgentAPI', version='0.1.0')
 app.include_router(router)
 
 if __name__ == '__main__':
-    uvicorn.run(app,port=SETTINGS.API_PORT)
-    
+    uvicorn.run(app, port=SETTINGS.API_PORT)
--- doc_agent_api/src/settings.py
+++ doc_agent_api/src/settings.py
@@ -1,15 +1,17 @@
-from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 load_dotenv()
+
+
 class Settings(BaseSettings):
     PROJECT_PATH: str
     OUTPUT_PATH: str
+    INPUT_PATH: str
     GOOGLE_API_KEY: str
-    MODEL: str 
+    MODEL: str
     API_PORT: int
     SYSTEM_INSTRUCTION: str
-    
+
 
 SETTINGS = Settings()
-    
