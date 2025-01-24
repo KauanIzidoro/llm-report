c265768bc0f35c3c81afb0f278959a33867dbd3e
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-24 13:16:31 -0300

    :wrench: chore: update schemas

--- doc_agent_api/src/schemas.py
+++ doc_agent_api/src/schemas.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
-import typing_extensions as typing
 
 class ModelInputPydantic(BaseModel):
     context_path: str
@@ -10,19 +9,6 @@ class ModelInputPydantic(BaseModel):
 class ModelOutputPydantic(BaseModel):
     input_path: str
     model_response: str
-    datetime: str
-    http_status: int
-    message: str
-    
-class ModelPrompt(typing.TypedDict):
-    context_path: str
-    output_path: str 
-    prompt: str 
-    
-    
-class ModelOutput(typing.TypedDict):
-    input_path: str
-    diagram_text: str
     datetime: str
     http_status: int
     message: str
