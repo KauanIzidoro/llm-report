1a2ce8362a8ec394451a0ff0943c876774b5d530
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-24 10:09:01 -0300

    :wrench: chore: linter format

--- doc_agent_api/mock_project/main.py
+++ doc_agent_api/mock_project/main.py
@@ -3,28 +3,27 @@ import time
 
 def calendar():
     months = [
-        "JAN",
-        "FEB",
-        "MAR",
-        "APR",
-        "MAY",
-        "JUN",
-        "JUL",
-        "AUG",
-        "SEP",
-        "OCT",
-        "NOV",
-        "DEC",
+        'JAN',
+        'FEB',
+        'MAR',
+        'APR',
+        'MAY',
+        'JUN',
+        'JUL',
+        'AUG',
+        'SEP',
+        'OCT',
+        'NOV',
+        'DEC',
     ]
     localdate = time.localtime()
     return {
-        "Year": localdate.tm_year,
-        "Month": months[localdate.tm_mon - 1],
-        "Day": localdate.tm_mday,
-        "Time": f"{localdate.tm_hour}:{localdate.tm_min}:{localdate.tm_sec}",
+        'Year': localdate.tm_year,
+        'Month': months[localdate.tm_mon - 1],
+        'Day': localdate.tm_mday,
+        'Time': f'{localdate.tm_hour}:{localdate.tm_min}:{localdate.tm_sec}',
     }
 
 
 def xyz():
-    return "xyz"
-
+    return 'xyz'
