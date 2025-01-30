723663fce279d0602121f326b360d4cf9ca05ff5
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-01-30 03:05:54 -0300

    🔧 chore: update env variables

--- doc_agent_api/src/settings.py
+++ doc_agent_api/src/settings.py
@@ -2,16 +2,13 @@ from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 load_dotenv()
-
-
 class Settings(BaseSettings):
-    PROJECT_PATH: str
-    FILE_PATH: str
+    USER_INPUT_PATH: str
+    USER_FILE_PATH: str
     OUTPUT_PATH: str
-    INPUT_PATH: str
     GOOGLE_API_KEY: str
     MODEL: str
-    API_PORT: int
+    API_PORT: str
     SYSTEM_INSTRUCTION: str
 
 
